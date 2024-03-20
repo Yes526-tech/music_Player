43d42060be371bcff5ad961232a26c1f8061665b
Emre <57071956+Yes526-tech@users.noreply.github.com>
2024-03-20 17:18:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,64 @@
 const image = document.getElementById("cover"),
     title = document.getElementById("music-title"),
-    artist  = document.getElementById("music-artist"),
-    currentTimeEl  = document.getElementById("current-time"),
-    durationEl  = document.getElementById("duration"),
-    progress  = document.getElementById("progress"),
-    playerProgress  = document.getElementById("player-progress"),
-    prevBtn  = document.getElementById("prev"),
-    nextBtn  = document.getElementById("next"),
-    playBtn  = document.getElementById("play"),
-    background  = document.getElementById("bg-img");
-
-
+    artist = document.getElementById("music-artist"),
+    currentTimeEl = document.getElementById("current-time"),
+    durationEl = document.getElementById("duration"),
+    progress = document.getElementById("progress"),
+    playerProgress = document.getElementById("player-progress"),
+    prevBtn = document.getElementById("prev"),
+    nextBtn = document.getElementById("next"),
+    playBtn = document.getElementById("play"),
+    background = document.getElementById("bg-img");
 
 const music = new Audio();
-
-
 
 const songs = [
     {
         path: "assets/1.mp3",
         displayName: "1",
-        cover:"assets/1.jpeg",
+        cover: "assets/1.jpeg",
         artist: "Yunus",
     },
     {
         path: "assets/2.mp3",
         displayName: "2",
-        cover:"assets/2.jpeg",
+        cover: "assets/2.jpeg",
         artist: "emre",
     },
     {
         path: "assets/3.mp3",
         displayName: "3",
-        cover:"assets/3.jpeg",
+        cover: "assets/3.jpeg",
         artist: "sert",
     }
-]
-
+];
 
 let musicIndex = 0;
 let isPlaying = false;
 
-
-function tooglePlay() {
-    if(isPlaying) {
+function togglePlay() {
+    if (isPlaying) {
         pauseMusic();
-    }else {
+    } else {
         playMusic();
     }
 }
 
-
 function playMusic() {
     isPlaying = true;
-    playBtn.classlist.replace("fa-play", "fa-pause");
+    playBtn.classList.replace("fa-play", "fa-pause");
     playBtn.setAttribute("title", "Pause");
     music.play();
 }
+
 function pauseMusic() {
     isPlaying = false;
-    playBtn.classlist.replace("fa-pause", "fa-play");
+    playBtn.classList.replace("fa-pause", "fa-play");
     playBtn.setAttribute("title", "Play");
     music.pause();
 }
 
-function loadMusic(song){
+function loadMusic(song) {
     music.src = song.path;
     title.textContent = song.displayName;
     artist.textContent = song.artist;
@@ -72,38 +66,35 @@ function loadMusic(song){
     background.src = song.cover;
 }
 
-
-function changeMusic(direction){
+function changeMusic(direction) {
     musicIndex = (musicIndex + direction + songs.length) % songs.length;
     loadMusic(songs[musicIndex]);
-    playMusic();
+    if (isPlaying) {
+        playMusic();
+    }
 }
 
-
-function updateProgressBar(){
-    const {duration, currentTime} = music;
+function updateProgressBar() {
+    const { duration, currentTime } = music;
     const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = '${progressPercent}%';
+    progress.style.width = `${progressPercent}%`;
 
     const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
-    durationEl.textContent = '${formatTime(duration / 60)}:${formatTime(duration % 60)}';
-    currentTimeEl.textContent = '${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}';
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
 }
 
-
-
-function setProgressBar(e){
+function setProgressBar(e) {
     const width = playerProgress.clientWidth;
     const clickX = e.offsetX;
     music.currentTime = (clickX / width) * music.duration;
 }
 
-playBtn.addEventListener("click", tooglePlay);
+playBtn.addEventListener("click", togglePlay);
 prevBtn.addEventListener("click", () => changeMusic(-1));
 nextBtn.addEventListener("click", () => changeMusic(1));
 music.addEventListener("ended", () => changeMusic(1));
 music.addEventListener("timeupdate", updateProgressBar);
 playerProgress.addEventListener("click", setProgressBar);
-
 
 loadMusic(songs[musicIndex]);
